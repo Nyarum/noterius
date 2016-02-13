d21ac0b4b9889f405c418d44b9ceba514d645734
Nyarum <puri.world@yandex.ru>
2016-02-13 05:33:53 +0300

    Updated tests

--- packet/packet_test.go
+++ packet/packet_test.go
@@ -4,13 +4,17 @@ import (
 	"testing"
 
 	"github.com/Nyarum/noterius/core"
+	"github.com/Nyarum/noterius/database"
 	"github.com/Nyarum/noterius/entitie"
+	"github.com/Nyarum/noterius/manager"
 )
 
 func BenchmarkEncode940Packet(b *testing.B) {
 	buffers := core.NewBuffers()
 	player := entitie.NewPlayer(buffers)
-	packet := NewPacket(player)
+	database := database.NewDatabase(&core.DatabaseInfo{})
+	manager := manager.NewManager(database)
+	packet := NewPacket(player, manager)
 
 	for n := 0; n < b.N; n++ {
 		packet.Encode(940)
@@ -20,7 +24,9 @@ func BenchmarkEncode940Packet(b *testing.B) {
 func BenchmarkEncode931Packet(b *testing.B) {
 	buffers := core.NewBuffers()
 	player := entitie.NewPlayer(buffers)
-	packet := NewPacket(player)
+	database := database.NewDatabase(&core.DatabaseInfo{})
+	manager := manager.NewManager(database)
+	packet := NewPacket(player, manager)
 
 	for n := 0; n < b.N; n++ {
 		packet.Encode(931)
